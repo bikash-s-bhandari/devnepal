74c922a0368e108a3ce4c0afd3b6c3b19d156e2b
Bikash Bhandari <bikash.bhandari05@gmail.com>
2021-01-18 12:40:20 +0545

    mongo atlas connected

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+const connectDb = require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 app.get('/', (req, res) => {
     res.status(200).send('Api running')
 
